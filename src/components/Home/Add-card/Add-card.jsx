17c04f9eeb62c920706489c5bd1b8ed0e0124c38
Vlad Tkachov <vlad.miakshin@gmail.com>
2018-04-09 15:43:39 +0300

    changed colors for increase readability of text

--- src/components/Home/Add-card/Add-card.jsx
+++ src/components/Home/Add-card/Add-card.jsx
@@ -7,7 +7,7 @@ export default function addCard(props){
   return(
     <div className="weather-card-create">
       <header className="weather-card-create__header">
-        <h2 className="weather-card-create__header">Add new card</h2>
+        <h2 className="weather-card-create__title">Add new card</h2>
       </header>
       <div className="weather-card-create__main">
         <label className="weather-card-create__label">Input your city:</label>
